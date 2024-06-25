bdbb1ffebe368844b1b7196a2065a5988e3ad34d
brunojose0 <brunojose1@unochapeco.edu.br>
2024-06-24 22:27:19 -0300

    projeto unity completo

--- MENU/Assets/SpaceShip.cs
+++ MENU/Assets/SpaceShip.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SpaceShip : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D rb;
+
+    public float movementSpeed = 5f;
+    public float rotationSpeed = 160f;
+
+    public GameObject bulletPrefab;
+
+    public Restarter restarter;
+
+    float rotation;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+
+        rb.AddForce( ((Vector2)transform.up * v * movementSpeed) - rb.velocity, ForceMode2D.Force);
+    }
     void Update()
     {
-        
+        float rotationDir = 0f;
+        if (Input.GetKey(KeyCode.A))
+            rotationDir = 1f;
+        else if (Input.GetKey(KeyCode.D))
+            rotationDir = -1f;
+        rotation += rotationDir * Time.smoothDeltaTime * rotationSpeed;
+
+        transform.localEulerAngles = new Vector3(0f, 0f, rotation);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            var bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Asteroid")
+        {
+            restarter.Restart();
+            gameObject.SetActive(false);
+        }
     }
 }
